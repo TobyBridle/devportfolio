1a72c27089fea78244a9ceb41d3d731473b6b719
Toby Bridle <foreignspaghettibolognese@gmail.com>
2025-02-09 13:00:54 +0000

    feat: add meta tags

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,6 +3,13 @@
 	let { children } = $props();
 </script>
 
+<svelte:head>
+	<meta property="og:title" content="Toby Bridle's Portfolio" />
+	<meta property="og:url" content="https://trika.gay" />
+	<meta property="og:description" content="(◕‿◕✿)" />
+	<meta property="og:image" content="https://picsum.photos/400" />
+</svelte:head>
+
 {@render children()}
 
 <style>
